4d87f844e04317442954ee681deff3d49faf0995
TulaevaLiza <pankova_liza@mail.ru>
2017-08-28 12:37:56 +0300

    добавлен скрипт для адаптивного меню

--- my_js/main.js
+++ my_js/main.js
@@ -1,5 +1,11 @@
-	$("#form3 button[type='submit']").click(function() {
-		var formObj=new MyForm("form3");
-		formObj.getForm();
-		formObj.submit();	
+$(document).ready(function () {
+
+
+//для адаптивного меню - показывать меню по нажатию на кнопку
+	$('.navbar-toggle').click(function() {
+		var menu=$(this).attr("data-target");
+		var cl=$(this).attr("data-toggle");
+		$(menu).toggleClass(cl);		
 	});
+
+})
